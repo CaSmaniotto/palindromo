c47dd4d628e9337bae5e47474ee8fd538802cf5e
Caetano Smaniotto <smaniottocaetano@gmail.com>
2023-06-23 15:08:27 -0300

    Update palindromo.py
    Pequenas alterações e arrumado duplo print quando a palavra é um palíndromo.

--- palindromo.py
+++ palindromo.py
@@ -1,30 +1,31 @@
 def palindromo(palavra, ini, fin, meio):
     # caso de parada
-    if ini < meio:
-        if fin < meio:
+    if (ini < meio and fin > meio):
 
-            # se a letra no indice ini for igual a letra no indice fin, será feita chamada recusiva...
-            if palavra[ini] == palavra[fin]:
-                ini += 1
-                fin -= 1
-                palindromo(palavra, ini, fin, meio)
+        # se a letra no indice ini for igual a letra no indice fin, será feita chamada recusiva...
+        if palavra[ini] == palavra[fin]:
+            ini += 1
+            fin -= 1
+            # chamada recursiva
+            palindromo(palavra, ini, fin, meio)
+            return 0
 
-            # se forem diferentes a palavra não é palindromo
-            else:
-                print("Não é palindromo!")
-                return 0
+        # se forem diferentes a palavra não é palindromo
+        else:
+            print("Não é palindromo!")
+            return 0
+
     print("É palindromo")
-    return 1
+    return 0
 
 
 palavra = input("Digite a palavra: ")
 
-tam = 0
-
 # descobrindo o tamanho da palavra
-for i in range(len(palavra)):
-    tam += 1
+ultimo_char = len(palavra)
 
 # numero que corresponde a letra do meio
-meio = tam // 2
-palindromo(palavra, 0, -1, meio)
+meio = len(palavra) // 2
+
+# call function
+palindromo(palavra.lower(), 0, ultimo_char-1, meio)
